fd07217d0cace8244d7f6dd93308b3963d7f33b7
pretasouzamirandadesouza-maker <pretasouzamirandadesouza@gmail.com>
2026-04-22 22:47:37 -0300

    Fix package declaration in SeekArc.java

--- app/src/main/java/com/aurora/launcher/gui/util/SeekArc.java
+++ app/src/main/java/com/aurora/launcher/gui/util/SeekArc.java
@@ -21,7 +21,7 @@
  * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  ******************************************************************************/
-package com.touch.mobile.dark.gui.util;
+package com.aurora.launcher.gui.util;
 
 import android.content.Context;
 import android.content.res.Resources;
